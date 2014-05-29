dbff1a3dfa3c876c02e0e796b32818372abda01a
CoCo2Git <git@codecombat.com>
2014-05-29 08:44:56 -0700

    -

---
+++ LevelComponent/programming/LimitsExecution/code.coffee
@@ -0,0 +1,4 @@
+class LimitsExecution extends Component
+  @className: "LimitsExecution"
+  chooseAction: ->
+    @attack @
---
+++ LevelComponent/programming/LimitsExecution/full.js
@@ -0,0 +1,43 @@
+{
+	"_id": "538755f8cb18e700007122bc",
+	"index": true,
+	"slug": "limitsexecution",
+	"name": "LimitsExecution",
+	"js": "var LimitsExecution, _ref,\n  __hasProp = {}.hasOwnProperty,\n  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };\n\nLimitsExecution = (function(_super) {\n  __extends(LimitsExecution, _super);\n\n  function LimitsExecution() {\n    _ref = LimitsExecution.__super__.constructor.apply(this, arguments);\n    return _ref;\n  }\n\n  LimitsExecution.className = \"LimitsExecution\";\n\n  LimitsExecution.prototype.chooseAction = function() {\n    return this.attack(this);\n  };\n\n  return LimitsExecution;\n\n})(Component);\n",
+	"creator": "512ef4805a67a8c507000001",
+	"original": "5367aaec1415c79e648b1deb",
+	"watchers": [
+		"512ef4805a67a8c507000001",
+		"5162fab9c92b4c751e000274"
+	],
+	"__v": 0,
+	"commitMessage": "",
+	"parent": "538697dccb18e7000071225a",
+	"official": false,
+	"configSchema": {
+		"additionalProperties": false,
+		"type": "object"
+	},
+	"dependencies": [],
+	"description": "This Component makes the Thang attack itself.",
+	"system": "programming",
+	"codeLanguage": "coffeescript",
+	"searchStrings": "limits execution l li lim limi limit limits limitse limitsex limitsexe limitsexec limitsexecu limitsexecut limitsexecuti",
+	"created": "2014-05-29T15:44:56.951Z",
+	"version": {
+		"isLatestMinor": true,
+		"isLatestMajor": true,
+		"minor": 3,
+		"major": 0
+	},
+	"permissions": [
+		{
+			"access": "owner",
+			"target": "512ef4805a67a8c507000001"
+		},
+		{
+			"access": "read",
+			"target": "public"
+		}
+	]
+}
