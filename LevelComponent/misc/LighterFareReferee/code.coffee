d024db5d40a09338f7a1673ba40a5d6338f7ed18
CoCo2Git <git@codecombat.com>
2015-01-20 10:41:19 -0800

    Adding a custom Referee stub.

---
+++ LevelComponent/misc/LighterFareReferee/code.coffee
@@ -0,0 +1,12 @@
+class LighterFareReferee extends Component
+  @className: 'LighterFareReferee'
+  
+  setUpLevel: ->
+    # Can do custom init here
+    
+    
+  chooseAction: ->
+    # Can do custom per-frame stuff here
+    
+    
+  
---
+++ LevelComponent/misc/LighterFareReferee/full.js
@@ -0,0 +1,42 @@
+{
+	"_id": "54bea14fe59db8506a049271",
+	"searchStrings": "lighter fare referee l li lig ligh light lighte lighter lighterf lighterfa lighterfar lighterfare lighterfarer lighterfarere lighterfareref lighterfarerefe lighterfarerefer",
+	"index": true,
+	"slug": "lighterfarereferee",
+	"name": "LighterFareReferee",
+	"js": "var LighterFareReferee, _ref,\n  __hasProp = {}.hasOwnProperty,\n  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };\n\nLighterFareReferee = (function(_super) {\n  __extends(LighterFareReferee, _super);\n\n  function LighterFareReferee() {\n    _ref = LighterFareReferee.__super__.constructor.apply(this, arguments);\n    return _ref;\n  }\n\n  LighterFareReferee.className = 'LighterFareReferee';\n\n  LighterFareReferee.prototype.setUpLevel = function() {};\n\n  LighterFareReferee.prototype.chooseAction = function() {};\n\n  return LighterFareReferee;\n\n})(Component);\n",
+	"system": "misc",
+	"creator": "512ef4805a67a8c507000001",
+	"original": "54bea0b9e59db8506a049270",
+	"watchers": [
+		"512ef4805a67a8c507000001",
+		"5162fab9c92b4c751e000274"
+	],
+	"__v": 0,
+	"description": "This Component makes the Thang o'ermaster the Lighter Fare level.",
+	"dependencies": [
+		{
+			"original": "54977ce657e90bd1903dea72",
+			"majorVersion": 0
+		}
+	],
+	"commitMessage": "Adding a custom Referee stub.",
+	"parent": "54bea0b9e59db8506a049270",
+	"created": "2015-01-20T18:41:19.589Z",
+	"version": {
+		"isLatestMinor": true,
+		"isLatestMajor": true,
+		"minor": 1,
+		"major": 0
+	},
+	"permissions": [
+		{
+			"access": "owner",
+			"target": "512ef4805a67a8c507000001"
+		},
+		{
+			"access": "read",
+			"target": "public"
+		}
+	]
+}
